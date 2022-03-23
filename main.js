590422c7abfb02f30eada3f3e8e6f2a42f036356
jhnbg rth <I1234itsplaz@outlook.com>
2022-03-23 12:57:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-
+var openTab = 1;
+function openFarmsMenu() {
+ if (openTab !== 1) {
+   
+ }
+}
+function openUpgrdsMenu() {
+  if (openTab !== 2) {
+   
+ }
+}
+function openAchMenu() {
+  if (openTab !== 3) {
+   
+ }
+}
+function openDrugsMenu() {
+  if (openTab !== 4) {
+   
+ }
+}
