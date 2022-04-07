8fac5cad5adbbae1b6c033b872c879b3db8c057f
jhnbg rth <I1234itsplaz@outlook.com>
2022-04-07 08:04:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -75,14 +75,14 @@ var autoBuyerIntervals = {
  'boost':'off'
 }
 var autobuyerMults = {
- '1':1
- '2':1
- '3':1
- '4':1
- '5':1
- '6':1
- '7':1
- '8':1
+ '1':1,
+ '2':1,
+ '3':1,
+ '4':1,
+ '5':1,
+ '6':1,
+ '7':1,
+ '8':1,
  'boost':1
 }
 var game = JSON.parse(localStorage.getItem('gameSave'));
